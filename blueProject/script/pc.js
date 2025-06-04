696472d8ac794ccc05c64c59bf64a9154fbc5447
소은경 <83527824+kyung0446@users.noreply.github.com>
2025-06-04 17:41:43 +0900

    change
    change

--- blueProject/script/pc.js
+++ blueProject/script/pc.js
@@ -80,23 +80,85 @@ $keywordList.forEach((elem,idx)=>{
   });
 });
 
+//project 이동 gsap 애니
+const fromTop = (elem,posY)=>{
+  gsap.from(elem,{
+    y:200,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger:{
+      trigger: elem,
+      start: `top ${posY}%`,
+      end: "top 25%",
+      scrub: true
+    }
+  });
+}
 // project 안에 card item을 계단 형식으로 애니처리 됨
 const $project = document.querySelectorAll("#projects>.nomal");
 $project.forEach((article)=>{
   const $item = article.querySelectorAll(".item");
   $item.forEach((item,idx)=>{
     let posY = 90 - idx*15;
-    gsap.from(item,{
-      y:200,
-      opacity: 0,
-      duration: 1,
-      scrollTrigger:{
-        trigger: item,
-        start: `top ${posY}%`,
-        end: "top 25%",
-        scrub: true
-      }
-    });
+    fromTop(item,posY);
   });
 });
+
+//practice project 영역은 계단식 애니메이션 처리
+const $practice = document.querySelectorAll("#projects>.practice .item");
+$practice.forEach((item,idx)=>{
+  let posY = 70 - idx*3;
+  fromTop(item,posY);
+});
+
+//skills 에서 h2태그는 커진상태에서 작아지면서 안보이게
+gsap.to("#skills>h2",{
+  opacity: 0,
+  scale: 0.5,
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#skills",
+    start: "top 80%",
+    toggleActions: "play reverse play reverse"
+  }
+});
+//skills에서 li들은 작아진 상태에서 커지게 : stagger
+const $shapes = document.querySelectorAll(".skill-item>li");
+gsap.from($shapes,{
+  opacity: 0,
+  scale:0.2,
+  duration: 0.5,
+  stagger: 0.2,
+  ease: "back.out",
+  scrollTrigger:{
+    trigger: "#skills",
+    start: "top 50%",
+    toggleActions: "play reverse play reverse"
+  }
+});
+
+//footer 에서 아이템들이 커졌다가 원상태로 복귀
+const $footer = document.querySelectorAll(".footer-wrap > *");
+gsap.from($footer,{
+  scale: 1.3,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.1,
+  ease: "power2.out",
+  scrollTrigger:{
+    trigger: "footer",
+    start: "top 20%",
+    toggleActions: "play reverse play reverse"
+  }
+});
+
+//home 을 누르면 제일 위로 올라가기
+const $home = document.querySelector("#logo");
+$home.addEventListener("click",()=>{
+  window.scrollTo({
+    top: 0
+  });
+});
+
+
 
